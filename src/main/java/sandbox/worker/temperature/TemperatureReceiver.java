10f91460b770a14424f4f4dcf698d2e13070d454
lorenzodemicheli <lorenzo.demicheli@bbv.ch>
2018-08-23 12:06:14 +0200

    improved

--- src/main/java/sandbox/worker/temperature/TemperatureReceiver.java
+++ src/main/java/sandbox/worker/temperature/TemperatureReceiver.java
@@ -1,5 +1,7 @@
 package sandbox.worker.temperature;
 
+import org.apache.logging.log4j.Logger;
+import org.elasticsearch.common.logging.Loggers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
@@ -9,14 +11,16 @@ import sandbox.repositories.TemperatureMeasureRepository;
 @Component
 public class TemperatureReceiver {
 
+    protected final Logger logger = Loggers.getLogger(TemperatureReceiver.class);
+
 
     @Autowired
     TemperatureMeasureRepository temperatureMeasureRepository;
 
 
-    @JmsListener(destination = "${temperature.topic}")
+    @JmsListener(destination = "${temperature.topic}", concurrency = "${temperature.concurrency}")
     public void receive(TemperatureMeasure temperatureMeasure) {
-        System.err.println(temperatureMeasure);
+        logger.info("Received Temperature: "+ temperatureMeasure);
         temperatureMeasureRepository.save(temperatureMeasure);
     }
 }
